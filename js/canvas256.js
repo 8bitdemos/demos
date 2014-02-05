6e2a792f934f988840aaea0c8884c5a260f6da20
8bitdemos <8bitdemos@gmail.com>
2014-02-05 21:51:41 +0200

    pentagram

--- js/canvas256.js
+++ js/canvas256.js
@@ -120,7 +120,31 @@ Canvas256.prototype = (function() {
 		},
 
 		line: function(x0, y0, x1, y1, color) {
-			//TODO
+			// bresenham's line algorithm from wikipedia
+
+			var dx = Math.abs(x1 - x0);
+			var dy = Math.abs(y1 - y0);
+			var sx = (x0 < x1) ? 1 : - 1;
+			var sy = (y0 < y1) ? 1 : - 1;
+			var err = dx - dy;
+
+			while (true) {
+				this.putPixel(x0, y0, color);
+				if (x0 === x1 && y0 === y1) break;
+				var e2 = 2 * err;
+				if (e2 > -dy) {
+					err -= dy;
+					x0 += sx;
+				}
+				if (x0 === x1 && y0 === y1) {
+					this.putPixel(x0, y0, color);
+					break;
+				}
+				if (e2 < dx) { 
+					err += dx;
+					y0 += sy;
+				}
+			}
 		},
 
 		clear: function(color) {					
