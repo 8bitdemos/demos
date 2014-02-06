9c7bdfc401736ecd834799406191b05b9e6d834e
8bitdemos <8bitdemos@gmail.com>
2014-02-06 17:02:41 +0200

    add yellow shape to pentagram.js

--- js/pentagram.js
+++ js/pentagram.js
@@ -9,11 +9,11 @@ var dsp;
 var RED = 0;
 var BLUE = 1;
 var GREEN = 2;
+var YELLOW = 3;
 var mode = RED;
 
 function init() {
 	cv = new Canvas256();
-	//cv.fitToWindow = false;
 	cv.init();
 
 	handleModeSwitch();
@@ -54,11 +54,22 @@ function handleModeSwitch() {
 			cv.setPaletteColor(i + 64 * 3, 255, 255, 255);
 		}	
 	}
+	else
+	if (mode === YELLOW) {
+		for (var i = 0; i < 64; i++) {
+			cv.setPaletteColor(i + 64 * 0, i * 2, i * 2, 0);
+			cv.setPaletteColor(i + 64 * 1, 128 + i * 2, 128 + i * 2, 0);
+			cv.setPaletteColor(i + 64 * 2, 255, 255, i * 4);
+			cv.setPaletteColor(i + 64 * 3, 255, 255, 255);
+		}	
+	}
 }
 
 var angle = 0;
 var radius = 60;
+var radius2 = 30;
 var delta = 0.05;
+var delta2 = 0.05;
 var deltaAngle = 1;
 function draw() {
 	//cv.clear();
@@ -67,30 +78,50 @@ function draw() {
 	if (radius >= 70 || radius <= 50) 
 	{ 
 		delta *= -1;
-		if (mode === RED) mode = BLUE; else
-		if (mode === BLUE) mode = GREEN; else
-		if (mode === GREEN) mode = RED;
+		mode++;
+		if (mode === 4) mode = 0;		
 		handleModeSwitch();
+	}
+
+	radius2 += delta2;
+	if (radius2 >= 70 || radius2 <= 20) {
+		delta2 *= -1;
 	}
 
 	angle += deltaAngle;
 
-	for (var i = 0; i < 5; i++) {
-		var x0 = Math.floor(160 + radius * Math.cos(Math.PI / 180 * (angle + 72 * i)));
-		var y0 = Math.floor(90 + radius * Math.sin(Math.PI / 180 * (angle + 72 * i)));
-		var x1 = Math.floor(160 + radius * Math.cos(Math.PI / 180 * (angle + 72 * (i + 2))));
-		var y1 = Math.floor(90 + radius * Math.sin(Math.PI / 180 * (angle + 72 * (i + 2))));
+	if (mode === RED || mode === GREEN) {
+		for (var i = 0; i < 5; i++) {
+			var x0 = Math.floor(160 + radius * Math.cos(Math.PI / 180 * (angle + 72 * i)));
+			var y0 = Math.floor(90 + radius * Math.sin(Math.PI / 180 * (angle + 72 * i)));
+			var x1 = Math.floor(160 + radius * Math.cos(Math.PI / 180 * (angle + 72 * (i + 2))));
+			var y1 = Math.floor(90 + radius * Math.sin(Math.PI / 180 * (angle + 72 * (i + 2))));
 
-		line(x0, y0, x1, y1);
+			line(x0, y0, x1, y1);
+		}
+
+		for (var i = 0; i < 20; i++) {
+			var x0 = Math.floor(160 + radius * Math.cos(Math.PI / 180 * (angle + 18 * i)));
+			var y0 = Math.floor(90 + radius * Math.sin(Math.PI / 180 * (angle + 18 * i)));
+			var x1 = Math.floor(160 + radius * Math.cos(Math.PI / 180 * (angle + 18 * (i + 1))));
+			var y1 = Math.floor(90 + radius * Math.sin(Math.PI / 180 * (angle + 18 * (i + 1))));
+
+			line(x0, y0, x1, y1);
+		}
 	}
+	else 
+	if (mode === BLUE || mode === YELLOW) {
+		for (var i = 0; i < 10; i++) {
+			var x0 = Math.floor(160 + radius * Math.cos(Math.PI / 180 * (angle + 36 * i)));
+			var y0 = Math.floor(90 + radius * Math.sin(Math.PI / 180 * (angle + 36 * i)));
+			var x1 = Math.floor(160 + radius2 * Math.cos(Math.PI / 180 * (angle + 36 * (i + 3))));
+			var y1 = Math.floor(90 + radius2 * Math.sin(Math.PI / 180 * (angle + 36 * (i + 3))));
+			var x2 = Math.floor(160 + radius * Math.cos(Math.PI / 180 * (angle + 36 * (i + 6))));
+			var y2 = Math.floor(90 + radius * Math.sin(Math.PI / 180 * (angle + 36 * (i + 6))));
 
-	for (var i = 0; i < 20; i++) {
-		var x0 = Math.floor(160 + radius * Math.cos(Math.PI / 180 * (angle + 18 * i)));
-		var y0 = Math.floor(90 + radius * Math.sin(Math.PI / 180 * (angle + 18 * i)));
-		var x1 = Math.floor(160 + radius * Math.cos(Math.PI / 180 * (angle + 18 * (i + 1))));
-		var y1 = Math.floor(90 + radius * Math.sin(Math.PI / 180 * (angle + 18 * (i + 1))));
-
-		line(x0, y0, x1, y1);
+			line(x0, y0, x1, y1);
+			line(x1, y1, x2, y2);
+		}
 	}
 
 	flare();
@@ -100,13 +131,13 @@ function draw() {
 }
 
 function flare() {
-	if (mode === RED) {
+	if (mode === RED || mode == YELLOW) {
 		for (var j = 1; j < 179; j++) {
 			for (var i = 1; i < 319; i++) {
 				var c = cv.getPixel(i, j) + cv.getPixel(i - 1, j) + cv.getPixel(i + 1, j) + cv.getPixel(i, j + 1);
 				c = Math.floor(c / 4);
 				c *= dsp[j * 320 + i];
-				c = Math.floor(clamp(c, 0, 255));
+				c = Math.floor(cv.clamp(c, 0, 255));
 				cv.putPixel(i, j, c);
 			}
 		}
@@ -123,19 +154,13 @@ function flare() {
 	}
 }
 
-function clamp(x, min, max) {
-	if (x < min) return min;
-	if (x > max) return max;
-	return x;
-}
-
 function line(x0, y0, x1, y1, color) {
 	var colorFunc;
 	if (mode === RED) colorFunc = function() { return Math.floor(Math.random() * 255); }; else
-	if (mode === BLUE || mode === GREEN) colorFunc = function() { return 255; };
+	if (mode === BLUE || mode === GREEN || mode == YELLOW) colorFunc = function() { return 255; };
 
 	var pixelFunc;
-	if (mode === RED || mode === BLUE) pixelFunc = function() { cv.putPixel(x0, y0, c); }; else
+	if (mode === RED || mode === BLUE || mode === YELLOW) pixelFunc = function() { cv.putPixel(x0, y0, c); }; else
 	if (mode === GREEN) pixelFunc = function() { if (greenTimer % 20 > 10) cv.putPixel(x0, y0, c); };
 
 	// bresenham's line algorithm from wikipedia
